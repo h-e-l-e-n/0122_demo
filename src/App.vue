2c3683feb0db961c5a431b8bd674de2776612320
h-e-l-e-n <leeleilei07@gmail.com>
2025-01-23 19:31:17 +0800

    layout: adjust

--- src/App.vue
+++ src/App.vue
@@ -2,30 +2,42 @@
 import { RouterView } from 'vue-router'
 </script>
 <template>
-  <header>
-    <nav class="navbar">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="https://taiwan-bank.vercel.app/">台灣銀行代碼查詢</a>
-      </div>
-    </nav>
-  </header>
-  <body>
-    <RouterView />
-  </body>
-  <footer>
-    <nav class="navbar ps-2">
-      <div class="container-fluid">
-        <a class="navbar-brand fs-6" href="https://github.com/h-e-l-e-n">powered by h-e-l-e-n</a>
-      </div>
-    </nav>
-  </footer>
+  <div class="page-container">
+    <header>
+      <nav class="navbar">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="https://taiwan-bank.vercel.app/" style="color: white"
+            >台灣銀行代碼查詢</a
+          >
+        </div>
+      </nav>
+    </header>
+    <main>
+      <RouterView />
+    </main>
+    <footer>
+      <nav class="navbar ps-2">
+        <div class="container-fluid">
+          <a class="navbar-brand fs-6" href="https://github.com/h-e-l-e-n" style="color: white"
+            >powered by h-e-l-e-n</a
+          >
+        </div>
+      </nav>
+    </footer>
+  </div>
 </template>
 <style>
+.page-container {
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  min-height: 100vh;
+}
+main {
+  flex-grow: 1;
+}
 .navbar {
   background-color: #6d8777;
-}
-.navbar-brand {
-  color: white;
 }
 .container-fluid {
   justify-content: center !important;
